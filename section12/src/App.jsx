6dcc17edb721c4aff8c678009d6fe4e68bf48c80
minheekim <kimmh2142@gmail.com>
2025-02-22 12:14:41 +0900

    12.3 라우팅설정하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,8 +1,26 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Diary from './pages/Diary'
+import New from './pages/New'
+import Notfound from './pages/Notfound'
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 
+// Routes 컴포넌트 밖에 있는 요소는 항상 렌더링
+// Routes 컴포넌트 안에는 Route컴포넌트만 가능
 function App() {
-  return <>ssss</>
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
 }
 
 export default App
---
+++ section12/src/pages/Diary.jsx
@@ -0,0 +1,5 @@
+const Diary = () => {
+    return <div>Dairy</div>
+}
+
+export default Diary
