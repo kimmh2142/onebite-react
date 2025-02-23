dce01435dc3407525d6dc713d7fa2e8d5b003d71
minheekim <kimmh2142@gmail.com>
2025-02-23 19:51:00 +0900

    12.6 폰트,이미지,레이아웃 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,10 @@ import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
 
+import { getEmotionImage } from './util/get-emotion-image'
+// 이모지를 pubic 폴더안에 넣으면
+// <img src={"/emotion.png"} /> 식 경로로 불러온다.
+// but, vite가 제공하는 이미지 최적화 안됌
 
 function App() {
   const nav = useNavigate();
@@ -15,6 +19,13 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
